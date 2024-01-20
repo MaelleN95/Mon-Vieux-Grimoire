4a7235ebc1d3a7756e25f67d3f741dea092b625e
Maelle <maelle.nioche@gmail.com>
2024-01-20 23:56:26 +0100

    add pw & email validators

---
+++ Backend/middlewares/user-validators.js
@@ -0,0 +1,35 @@
+const emailValidator = require('email-validator');
+const pwValidator = require('password-validator');
+
+exports.checkEmail = (req, res, next) => {
+  // Check email input with email-validator
+  emailCheck = emailValidator.validate(req.body.email);
+  if (!emailCheck) {
+    return res.status(400).json({ message: 'Unauthorized e-mail format' });
+  }
+  next();
+};
+
+// Creation of a security password schema
+const pwSchema = new pwValidator();
+pwSchema
+  .is().min(5) // Min length 60
+  .is().max(60) // Max length 60
+  .has().uppercase() // Must have uppercase letters
+  .has().lowercase() // Must have lowercase letters
+  .has().digits(2) // Must have at least 2 digits
+  .has().not().spaces() // Shouldn't have spaces
+  .is().not().oneOf(
+    ['password', 
+    'azerty123', 
+    'azertyuiop'
+  ]);
+
+exports.checkPw = (req, res, next) => {
+  // Check password input with password-validator
+  pwCheck = pwSchema.validate(req.body.password);
+  if (!pwCheck) {
+    return res.status(400).json({ message: 'Unauthorized password format' });
+  }
+  next();
+};
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// Import middleware
+const validators = require('../middlewares/user-validators');
+
 // Import user controller
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post(
+  '/signup',
+  validators.checkEmail,
+  validators.checkPw,
+  userCtrl.signup
+);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
