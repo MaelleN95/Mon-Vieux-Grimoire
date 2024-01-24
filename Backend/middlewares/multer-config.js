977d5b080d4e5d1e26700e401660c794f9a2766b
Maelle <maelle.nioche@gmail.com>
2024-01-24 16:05:44 +0100

    management of accents and special symbols

--- Backend/middlewares/multer-config.js
+++ Backend/middlewares/multer-config.js
@@ -15,10 +15,26 @@ const storage = multer.diskStorage({
 
   // File name after saving :
   filename: (req, file, callback) => {
+    // management of accents and special symbols
+    const decodedName = Buffer.from(file.originalname, 'latin1')
+      .toString('utf-8')
+      .replace(/[éèëêÉÈÊË]/g, 'e')
+      .replace(/[àäâÀÃ]/g, 'a')
+      .replace(/[ùûüÙÛÜ]/g, 'u')
+      .replace(/[ïîìÏÎ]/g, 'i')
+      .replace(/[ôöòõÔ]/g, 'o')
+      .replace(/[çÇ]/g, 'c')
+      .replace(/[&~#€©§¶+=/!?£%;$,:*{[|`^\\@}()]/g, '');
+
     // Split to remove the extension, and remove the spaces
-    const name = file.originalname.split('.')[0].split(' ').join('_');
+    const name = decodedName.split('.')[0].split(' ').join('_');
+
     // Define extension with extension dictionary (variable MIME_TYPES)
     const extension = MIME_TYPES[file.mimetype];
+
+    // if (extension != 'png' || extension != 'jpg' || extension != 'webp') {
+    //   console.error("Le format n'est pas accepté !");
+    // }
 
     callback(null, name + Date.now() + '.' + extension);
   },
